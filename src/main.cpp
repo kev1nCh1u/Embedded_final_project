16f0cef3403741fe6a4069cfaac0a4e85249ad0d
KevinChiu <fsd83811@gmail.com>
2021-06-16 15:52:53 +0800

    joy control mode

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@
 Servo servo_x; // 建立SERVO物件
 Servo servo_y;
 int pose_x = 90, pose_y = 90;
-int pose_x_min = 80, pose_x_max = 100;
-int pose_y_min = 80, pose_y_max = 110;
+int pose_x_base = 87, pose_y_base = 97;
 int buff = 0;
-int gui_flag = 0, control_flag = 0;
+int gui_flag = 0, control_flag = 0, joy_flag = 0, input_z_flag = 0;
 int gui_x, gui_y;
+int joy_delay = 0;
 
 void setup()
 {
@@ -53,15 +53,15 @@ void loop()
         gui_flag++;
         if (gui_flag == 2)
         {
-          if(buff == 82) //real_82 virtual_86
+          if (buff == 82) //real_82 virtual_86
           {
             control_flag = 0;
           }
-          if(buff == 86)
+          if (buff == 86)
           {
             control_flag = 1;
           }
-          else if(buff != 86)
+          else if (buff != 86)
           {
             buff = 0;
             gui_flag = 0;
@@ -78,8 +78,8 @@ void loop()
           gui_y = buff;
           // Serial.println(gui_y, DEC);
 
-          pose_x = map(gui_x, 100, 0, pose_x_min, pose_x_max);
-          pose_y = map(gui_y, 100, 0, pose_y_min, pose_y_max);
+          pose_x = map(gui_x, 100, 0, pose_x_base - 10, pose_x_base + 10);
+          pose_y = map(gui_y, 100, 0, pose_y_base - 10, pose_y_base + 10);
           buff = 0;
           gui_flag = 0;
         }
@@ -91,18 +91,70 @@ void loop()
   /********************************************
   * joystick
   * ****************************************/
-  if(control_flag == 0)
+  if (control_flag == 0)
   {
     input_x = analogRead(JOYSTICK_X);
     input_y = analogRead(JOYSTICK_Y);
     input_z = digitalRead(JOYSTICK_Z);
-    pose_x = map(input_x, 1, 1023, pose_x_min, pose_x_max);
-    pose_y = map(input_y, 1023, 1, pose_y_min, pose_y_max);
+
+    if (input_z == 1 && input_z_flag == 0)
+    {
+      input_z_flag = 1;
+      if (joy_flag == 0)
+      {
+        joy_flag = 1;
+      }
+      else if (joy_flag == 1)
+      {
+        joy_flag = 0;
+        pose_x_base = pose_x;
+        pose_y_base = pose_y;
+      }
+    }
+    else if (input_z == 0 && input_z_flag == 1)
+    {
+      input_z_flag = 0;
+    }
+
+    if (joy_flag == 0)
+    {
+      pose_x = map(input_x, 1, 1023, pose_x_base - 10, pose_x_base + 10);
+      pose_y = map(input_y, 1023, 1, pose_y_base - 10, pose_y_base + 10);
+    }
+    else
+    {
+      if (joy_delay == 0)
+      {
+        if (input_x < 268)
+          pose_x--;
+        else if (input_x > 768)
+          pose_x++;
+
+        if (input_y < 268)
+          pose_y++;
+        else if (input_y > 768)
+          pose_y--;
+      }
+      joy_delay++;
+      if (joy_delay > 1000)
+        joy_delay = 0;
+    }
   }
+  // Serial.println(joy_flag);
 
   /********************************************
    * servo
    * ****************************************/
+  if (pose_x > 100)
+    pose_x = 100;
+  else if (pose_x < 80)
+    pose_x = 80;
+
+  if (pose_y > 100)
+    pose_y = 100;
+  else if (pose_y < 80)
+    pose_y = 80;
+
   servo_x.write(pose_x); //旋轉到
   servo_y.write(pose_y); //旋轉到
 
