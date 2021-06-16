8c80c90c8381fbea7d97a6762ae0e3d16ff186b1
KevinChiu <fsd83811@gmail.com>
2021-06-16 08:04:47 +0800

    control_mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <Servo.h> //載入函式庫，這是內建的，不用安裝
+#include <Servo.h>
 
 #define JOYSTICK_X 14 //x
 #define JOYSTICK_Y 15 //y
@@ -8,7 +8,8 @@
 Servo servo_x; // 建立SERVO物件
 Servo servo_y;
 int pose_x = 90, pose_y = 90;
-int buff = 0, flag = 0;
+int buff = 0;
+int gui_flag = 0, control_flag = 0;
 int gui_x, gui_y;
 
 void setup()
@@ -25,51 +26,76 @@ void setup()
 
 void loop()
 {
-  /********************************************
-   * joystick
-   * ****************************************/
   int input_x, input_y, input_z;
-  input_x = analogRead(JOYSTICK_X);
-  input_y = analogRead(JOYSTICK_Y);
-  input_z = digitalRead(JOYSTICK_Z);
-  // pose_y = map(input_x, 1, 1023, 80, 100);
-  // pose_x = map(1024-input_y, 1, 1023, 80, 100);
-  
-  
+
   /********************************************
    * serial
    * ****************************************/
   while (1)
   {
-    if (Serial.available() > 0) {
-      if(buff == 0)
-        buff = Serial.read();
+    if (Serial.available() > 0)
+    {
+      buff = Serial.read();
       // Serial.write(buff);
       // Serial.println(buff, DEC);
-      if(buff == 71){   //G
-        Serial.write(83);   //S
+      if (buff == 71 && gui_flag == 0) //G
+      {
+        Serial.write(83); //S
         Serial.write(pose_x);
         Serial.write(pose_y);
         buff = 0;
         break;
       }
-      else if(buff == 67 || flag != 0){    //C
-        flag++;
-        if(flag == 2)
-          gui_x = Serial.read();
-        else if(flag == 3){
-          gui_y = Serial.read();
-
+      else if (buff == 67 || gui_flag != 0) //C
+      {
+        gui_flag++;
+        if (gui_flag == 2)
+        {
+          if(buff == 82) //real_82 virtual_86
+          {
+            control_flag = 0;
+          }
+          if(buff == 86)
+          {
+            control_flag = 1;
+          }
+          else if(buff != 86)
+          {
+            buff = 0;
+            gui_flag = 0;
+            break;
+          }
+        }
+        else if (gui_flag == 3)
+        {
+          gui_x = buff;
           // Serial.println(gui_y, DEC);
-          
+        }
+        else if (gui_flag == 4)
+        {
+          gui_y = buff;
+          // Serial.println(gui_y, DEC);
+
           pose_y = map(gui_x, 100, 0, 80, 100);
           pose_x = map(gui_y, 100, 0, 80, 100);
           buff = 0;
-          flag = 0;
+          gui_flag = 0;
         }
         break;
       }
     }
+  }
+
+  /********************************************
+  * joystick
+  * ****************************************/
+  if(control_flag == 0)
+  {
+    input_x = analogRead(JOYSTICK_X);
+    input_y = analogRead(JOYSTICK_Y);
+    input_z = digitalRead(JOYSTICK_Z);
+    pose_y = map(input_x, 1, 1023, 80, 100);
+    pose_x = map(1024-input_y, 1, 1023, 80, 100);
   }
 
   /********************************************
